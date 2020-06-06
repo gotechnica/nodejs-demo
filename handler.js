2d927d7cd8228d4288df5835d96f7f4ea242d1a2
Hugo Burbelo <hburbelo@phone2action.com>
2020-06-05 22:07:43 -0400

    Get hello world app working

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
-'use strict';
 
-module.exports.hello = async event => {
+
+module.exports.get_schedule = async event => {
+  const name = event.queryStringParameters.name || "test";
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
+        message: 'Hello, ' + name
+      }
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
