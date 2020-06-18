1184caa55d2764e25c1b25a1d667e3203a65111c
Brian Chin <bchin@MacBook-Pro.fios-router.home>
2020-06-18 03:07:08 -0400

    Added notes into both the handle.js and the serveless.yml file

--- handler.js
+++ handler.js
@@ -3,15 +3,20 @@ const AWS = require('aws-sdk');
 
 const TABLE_NAME = 'schedule-demo';
 
+// Note we need to put in our AWS key everytime, if we have not put it in perm.
+// Use sls deploy to generate, and then sls invoke -f <function name> to 
+// Using sls deploy -f function name, when we've only edited one function lets us deploy much faster, then we need to invoke again
+// To debug use sls logs -f <function name>
+
 // Function to get the schedule found in the `schedule-demo` table
 module.exports.get_schedule = async event => {
-  const result = await db.scanItems({
+  const result = await db.scanItems({ // We scan the items (returns a JSON) and set it to the var result
     TableName: TABLE_NAME
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(result.Items)
+    body: JSON.stringify(result.Items) // We stringify the JSON object to be more readable
   };
 };
 
@@ -21,7 +26,7 @@ module.exports.get_event = async event => {
 
   const item = await db.getItem({
     TableName: TABLE_NAME,
-    Key: { id }
+    Key: { id } // Tries to find the id that we passed in (in the event)
   });
 
   return {
@@ -34,7 +39,7 @@ module.exports.get_event = async event => {
 module.exports.add_event = async event => {
   const body = JSON.parse(event.body);
 
-  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'}); // We use AWS sdk to get the info
 
   const params = {
     TableName: TABLE_NAME,
@@ -43,7 +48,7 @@ module.exports.add_event = async event => {
 
   // dynamically add post request body params to document
   Object.keys(body).forEach(k => {
-    params.Item[k] = {S: body[k]}
+    params.Item[k] = {S: body[k]} // Gets all the elements and puts it into the Items list with k as the col key, we can have as many elements as we want as long as we have the primary key.
   });
 
   // Call DynamoDB to add the item to the table
