12f966564a30dd78a9ad792c7c61a1ff2b5795be
anna-feng <anna.f5718@gmail.com>
2020-06-20 10:46:06 -0400

    Update handler.js
    Adding more comments to handler.js functions

--- handler.js
+++ handler.js
@@ -5,10 +5,12 @@ const TABLE_NAME = 'schedule-demo';
 
 // Function to get the schedule found in the `schedule-demo` table
 module.exports.get_schedule = async event => {
+  // Dynamodb scans items from 'TABLE_NAME' table and stores it as 'result'
   const result = await db.scanItems({
     TableName: TABLE_NAME
   });
 
+  // Returns status code 200 and JSON string of 'Items' property from 'result'
   return {
     statusCode: 200,
     body: JSON.stringify(result.Items)
@@ -17,13 +19,16 @@ module.exports.get_schedule = async event => {
 
 // Function to get a single event from the `schedule-demo` table
 module.exports.get_event = async event => {
+  // 'id' set to existing parameter id or "1"
   const id = event.queryStringParameters.id || "1";
 
+  // 'item' set to item with 'id' key from 'TABLE_NAME' table 
   const item = await db.getItem({
     TableName: TABLE_NAME,
     Key: { id }
   });
 
+  // Returns status code 200 and JSON string of 'Item' property from 'item'
   return {
     statusCode: 200,
     body: JSON.stringify(item.Item)
@@ -32,10 +37,13 @@ module.exports.get_event = async event => {
 
 // Function to add an event to the `schedule-demo` table
 module.exports.add_event = async event => {
+  // 'body' set to result of converting JSON output from event.body to JS value
   const body = JSON.parse(event.body);
 
+  // 'ddb' set to new DynamoDB object with specific API version
   const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
+  // 'params' set to contain 'TABLE_NAME' table and unset number of items
   const params = {
     TableName: TABLE_NAME,
     Item: {}
@@ -49,6 +57,7 @@ module.exports.add_event = async event => {
   // Call DynamoDB to add the item to the table
   const result = await ddb.putItem(params).promise();
 
+  // Returns status code 200 and JSON string of 'result'
   return {
     statusCode: 200,
     body: JSON.stringify(result)
